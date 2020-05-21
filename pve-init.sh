8ebd78732b3e7636121fe42f1aea11956e48fc47
kiwindows <none>
2020-05-22 01:54:04 +0200

    initial commit

---
+++ pve-init.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+echo "
+                                                                                
+                                                                                
+                                            /&@@@  .@@&                         
+                                     @@@@@@@@@@@   @@@@@@@@@@@@#                
+                                 @@@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@*           
+                              @@@@@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@@@@@         
+              .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@@@@@@@       
+             @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@@@@@@@@@(     
+            @@@@@(                 @@@@@@@@@   @@@@@@@@@@@@@@@@@@@@@@@@@@@@     
+           .        *@@@@@@@@,          @@@,  (@@@@@@@@@@@@@@@@@@@@@@@@@@@@&    
+           #@@@@@@@@@@@@@@@@@@@@@@@@@         @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    
+           @@@@@@@@@@@@@@@ @@@@@@@@@@@@@@      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@    
+           @@@@@@@@@@,      #@@@@@@@@@@@@@   @     &@@@@@@@@@@@@@@@@@@@@@@@     
+         @@@@                 .@@@@@@@@@@   @@@@@@&        .@@@@@@@@/.          
+        @@@                      @@@@@@@   ,@@@@@@@@@@@@@            %@@@@      
+      /@@,                           @@*   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+     *@@                                  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@         
+     @@                                         @@@@@@@@@@@@@@@@@@@@            
+    @@.                                        (@@@      @@@@@@@@               
+    @@                                         @@@/       @@@@@                 
+    @                                         (@@@        @@@                   
+                                        /@@@@@@@@@@      %@@@                   
+                                            @     ,@@@@@@@@@@                   
+                                                      @@@                       
+
+Script originaly made by KiWindows : https://kiwindows.fr , https://github.com/kiwindows
+
+The purpose of this script is to disable the enterprise-repos of Proxmox VE and add the
+free one. It also disable the subscription popup on the WebGUI.
+"
+
+read -p "Would you like to continue ? y/Y/yes/Yes n/N/no/No" opt
+case $opt in
+    y|Y|yes|Yes)
+        mv pve-enterprise.list old.list
+        sed '1s/./#&/' old.list >pve-enterprise.list
+        rm old.list
+        read -p "The list has been changed. Would you like to update and fully upgrade Proxmox VE ?" opt
+        case $opt in
+            y|Y|yes|Yes)
+                apt update && apt full-upgrade
+                ;;
+        sed -i.bak "s/data.status !== 'Active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+        systemctl restart pveproxy.service
+        ;;
+    n|N|no|No)
+        exit
+        ;;
+    *)
+        echo "No an expected answer. Abording."
+        exit
+        ;;
+esac
