f81b1a59db459a0882ffac85159a1c78cb9605d2
kiwindows <none>
2020-05-22 15:50:56 +0200

    using inline edit for sources.list

--- pve-init.sh
+++ pve-init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "
-                                                                                
-                                                                                
                                             /&@@@  .@@&                         
                                      @@@@@@@@@@@   @@@@@@@@@@@@#                
                                  @@@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@*           
@@ -27,7 +25,7 @@ echo "
 
 Script originaly made by KiWindows : https://kiwindows.fr , https://github.com/kiwindows
 
-pve-autofree version 1.0
+pve-autofree version 1.1
 
 The purpose of this script is to disable the enterprise-repos of Proxmox VE and add the
 free one. It also disable the subscription popup on the WebGUI.
@@ -45,10 +43,8 @@ case $opt in
         mv pve-enterprise.list old.list
         sed '1s/./#&/' old.list >pve-enterprise.list
         rm old.list
-        sed "
-
-        # Free Proxmox VE depot
-        deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list
+        cd /etc/apt/
+        { echo ;; echo '# Free Proxmox VE depot'; echo 'deb http://download.proxmox.com/debian/pve buster pve-no-subscription'; } >> sources.list
         read -p "The list has been changed. Would you like to update and fully upgrade Proxmox VE ?
 
         " optupdate
