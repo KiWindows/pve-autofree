aca776b39ffd9a06b6e4c1f6bf59035ee49cf08c
kiwindows <none>
2020-05-22 12:25:14 +0200

    fixed some text return, auto validate upgrade and return message when pveproxy restart

--- pve-init.sh
+++ pve-init.sh
@@ -27,7 +27,7 @@ echo "
 
 Script originaly made by KiWindows : https://kiwindows.fr , https://github.com/kiwindows
 
-pve-autofree version 0.2
+pve-autofree version 1.0
 
 The purpose of this script is to disable the enterprise-repos of Proxmox VE and add the
 free one. It also disable the subscription popup on the WebGUI.
@@ -36,7 +36,9 @@ Once the free repos have been installed, the pveproxy service will automatically
 and Proxmox VE will update and full upgrade
 "
 
-read -p "Would you like to continue ? y/Y/yes/Yes n/N/no/No" opt
+read -p "Would you like to continue ? y/Y/yes/Yes n/N/no/No
+
+" opt
 case $opt in
     y|Y|yes|Yes)
         cd /etc/apt/sources.list.d/
@@ -47,13 +49,16 @@ case $opt in
 
         # Free Proxmox VE depot
         deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list
-        read -p "The list has been changed. Would you like to update and fully upgrade Proxmox VE ?" optupdate
+        read -p "The list has been changed. Would you like to update and fully upgrade Proxmox VE ?
+        
+        " optupdate
         case $optupdate in
             y|Y|yes|Yes)
-                apt update && apt full-upgrade
+                apt update && apt full-upgrade -y
                 ;;
         esac
         sed -i.bak "s/data.status !== 'Active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+        echo "pveproxy.service restarting, you may refresh your tab in a few seconds..."
         systemctl restart pveproxy.service
         ;;
     n|N|no|No)
