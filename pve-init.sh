cf8fba6886f11b69339416559e883c46fa1f7e71
kiwindows <none>
2020-05-22 02:19:02 +0200

    added the add free pve depot, RATE MY COMMIT MESSAGES OR ELSE I COMMIT OOF

--- pve-init.sh
+++ pve-init.sh
@@ -33,7 +33,7 @@ The purpose of this script is to disable the enterprise-repos of Proxmox VE and 
 free one. It also disable the subscription popup on the WebGUI.
 
 Once the free repos have been installed, the pveproxy service will automatically be restarted
-and Proxmox VE will update and full upgrade.
+and Proxmox VE will update and full upgrade
 "
 
 read -p "Would you like to continue ? y/Y/yes/Yes n/N/no/No" opt
@@ -42,6 +42,9 @@ case $opt in
         mv pve-enterprise.list old.list
         sed '1s/./#&/' old.list >pve-enterprise.list
         rm old.list
+        echo "
+        # Free Proxmox VE depot
+        deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list
         read -p "The list has been changed. Would you like to update and fully upgrade Proxmox VE ?" opt
         case $opt in
             y|Y|yes|Yes)
